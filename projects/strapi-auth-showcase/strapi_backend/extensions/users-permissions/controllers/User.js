9d7409c7c4ad0ed2da43a266d1be6c287c560b51
Kevin Kühnemund <32802935+jabali2004@users.noreply.github.com>
2021-09-07 14:12:05 +0200

    Update User.js

--- projects/strapi-auth-showcase/strapi_backend/extensions/users-permissions/controllers/User.js
+++ projects/strapi-auth-showcase/strapi_backend/extensions/users-permissions/controllers/User.js
@@ -18,21 +18,39 @@ module.exports = {
     delete ctx.request.body.blocked;
     delete ctx.request.body.role;
 
+    // Get user service and model ref
+    const userService = strapi.plugins['users-permissions'].services.user;
+    const userModel = strapi.plugins['users-permissions'].models.user;
+
+    const emailExists = await userService.findOne(
+      {
+        email: ctx.request.body.email
+      },
+      []
+    );
+
+    const usernameExists = await userService.findOne(
+      {
+        username: ctx.request.body.username
+      },
+      []
+    );
+
     if (ctx.request.body.email === null) {
       delete ctx.request.body.email;
+    } else if (emailExists && emailExists.id !== id) {
+      return ctx.throw(403, 'Email already exists!');
     }
 
     if (ctx.request.body.username === null) {
       delete ctx.request.body.username;
+    } else if (usernameExists && usernameExists.id !== id) {
+      return ctx.throw(403, 'Username already exists!');
     }
 
     if (ctx.request.body.password === null) {
       delete ctx.request.body.password;
     }
-
-    // Get user service and model ref
-    const userService = strapi.plugins['users-permissions'].services.user;
-    const userModel = strapi.plugins['users-permissions'].models.user;
 
     if (ctx.request.body.password) {
       const passwordHashed = await strapi.plugins[
@@ -44,7 +62,12 @@ module.exports = {
       ctx.request.body.password = passwordHashed;
     }
 
-    let entity = await userService.update({ id }, ctx.request.body);
+    let entity = await userService.update(
+      {
+        id
+      },
+      ctx.request.body
+    );
 
     return sanitizeEntity(entity, {
       model: userModel
